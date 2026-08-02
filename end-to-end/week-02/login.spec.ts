256ae7cf776a232b49ac74087be22ed9f2a038b7
Michael Lynch Smith <m.lynchsmith@servicepower.com>
2026-08-02 16:42:11 +0100

    Login scenarios added

--- end-to-end/week-02/login.spec.ts
+++ end-to-end/week-02/login.spec.ts
@@ -15,3 +15,41 @@ test('Login authenticated user', async ({page}) => {
    
 });
 
+test('Invalid password login', async ({page})=>{
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('InvlaidPassword');
+
+    await page.getByRole('button', {name: 'Login'}).click();
+
+    await expect(page.locator('.error-message-container')).toBeVisible();
+    await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible;
+});
+
+test('Locked user details', async ({page})=>{
+    
+    await page.getByPlaceholder('Username').fill('locked_out_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+
+    await page.getByRole('button', {name: 'Login'}).click()
+
+    await expect(page.locator('.error-message-container')).toBeVisible();
+    await expect(page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible();
+})
+
+test('Missing username', async ({page}) => {
+
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', {name: 'Login'}).click();
+
+    await expect(page.getByText('Epic sadface: Username is required')).toBeVisible();
+    
+})
+
+test('Missing password', async ({page}) => {
+
+    await page.getByPlaceholder('Username').fill('Epic sadface: Password is required');
+    await page.getByRole('button', {name: 'Login'}).click();
+
+    await expect(page.getByText('Epic sadface: Password is required')).toBeVisible();
+})
